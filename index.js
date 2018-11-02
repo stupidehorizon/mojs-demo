ab3738e398ea19e45907be6552ead885134be904
stupidehorizon <122505231@qq.com>
2018-11-02 18:25:52 +0800

    代码格式调整

--- index.js
+++ index.js
@@ -7,38 +7,38 @@ import '@fortawesome/fontawesome-free/js/all.js';
 const iconButton = document.getElementById('icon-button');
 const icon = document.getElementById('icon');
 new Animocon(iconButton, {
-  tweens : [
+  tweens: [
     // burst animation
     new mojs.Burst({
-      parent: 			iconButton,
-      radius: 			{30:90},
-      count: 				6,
-      children : {
-        fill: 			'#C0C1C3',
-        opacity: 		0.6,
-        radius: 		15,
-        duration: 	1700,
-        easing: 		mojs.easing.bezier(0.1, 1, 0.3, 1)
+      parent: iconButton,
+      radius: { 30: 90 },
+      count: 6,
+      children: {
+        fill: '#C0C1C3',
+        opacity: 0.6,
+        radius: 15,
+        duration: 1700,
+        easing: mojs.easing.bezier(0.1, 1, 0.3, 1)
       }
     }),
     // ring animation
     new mojs.Shape({
-      parent: 		iconButton,
-      type: 			'circle',
-      radius: 		{0: 60},
-      fill: 			'transparent',
-      stroke: 		'#C0C1C3',
-      strokeWidth: {20:0},
-      opacity: 		0.6,
-      duration: 	700,
-      easing: 		mojs.easing.sin.out
+      parent: iconButton,
+      type: 'circle',
+      radius: { 0: 60 },
+      fill: 'transparent',
+      stroke: '#C0C1C3',
+      strokeWidth: { 20: 0 },
+      opacity: 0.6,
+      duration: 700,
+      easing: mojs.easing.sin.out
     }),
     // icon scale animation
     new mojs.Tween({
-      duration : 1200,
-      onUpdate: function(progress) {
-        if(progress > 0.3) {
-          var elasticOutProgress = mojs.easing.elastic.out(1.43*progress-0.43);
+      duration: 1200,
+      onUpdate: function (progress) {
+        if (progress > 0.3) {
+          var elasticOutProgress = mojs.easing.elastic.out(1.43 * progress - 0.43);
           icon.style.WebkitTransform = icon.style.transform = 'scale3d(' + elasticOutProgress + ',' + elasticOutProgress + ',1)';
         }
         else {
@@ -47,11 +47,11 @@ new Animocon(iconButton, {
       }
     })
   ],
-  onCheck : function() {
+  onCheck: function () {
     iconButton.style.color = '#988ADE';
   },
-  onUnCheck : function() {
-    iconButton.style.color = '#C0C1C3';	
+  onUnCheck: function () {
+    iconButton.style.color = '#C0C1C3';
   }
 });
 
